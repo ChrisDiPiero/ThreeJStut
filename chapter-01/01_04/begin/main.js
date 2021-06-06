e03903f675feedcedc92f25be44fd363effc4442
Chris DiPiero <frogmaster99@msn.com>
2021-06-06 11:13:08 -0500

    moved camera to see box

--- chapter-01/01_04/begin/main.js
+++ chapter-01/01_04/begin/main.js
@@ -1,16 +1,8 @@
 function init() {
-	const scene = new THREE.Scene();
+  const scene = new THREE.Scene;
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1); //basic shape in 3js, this is cube
-  const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00
-  });//mesh basic material is unaffected by lighting
-  const mesh = new THREE.Mesh(
-    geometry, //pass geometry and material vars to mesh to create object
-    material
-  );
-
-  scene.add(mesh);
+  const box = getBox(1, 1, 1);
+  scene.add(box);
 
 	const camera = new THREE.PerspectiveCamera(
 		45,
@@ -18,6 +10,8 @@ function init() {
 		1,
 		1000
 	);
+  camera.position.z = 5; //box wasn't showing up. all origins are at 0,0,0. X and y normal cartesian, z is "depth" from user perspective
+
 	const renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.getElementById('webgl').appendChild(renderer.domElement);
@@ -25,6 +19,18 @@ function init() {
 		scene,
 		camera 
 	);
+}
+
+function getBox(w, h, d) {
+  const geometry = new THREE.BoxGeometry(w, h, d); //basic shape in 3js, this is cube if passing 1, 1, 1
+  const material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00
+  });//mesh basic material is unaffected by lighting
+  const mesh = new THREE.Mesh(
+    geometry, //pass geometry and material vars to mesh to create object
+    material
+  );
+  return mesh
 }
 
 init();

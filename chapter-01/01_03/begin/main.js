7cd2748ab914708b1a005ad44da72fcfe8aad8b8
Chris DiPiero <frogmaster99@msn.com>
2021-06-05 19:17:27 -0500

    set up renderer

--- chapter-01/01_03/begin/main.js
+++ chapter-01/01_03/begin/main.js
@@ -1,1 +1,21 @@
-console.log(THREE);
+function init() {
+const scene = new THREE.Scene(); //container for all objects this IS the world
+
+  const camera = new THREE.PerspectiveCamera( //also need a camera for POV
+    45, // field of view. how much of the Scene
+    window.innerWidth/window.innerHeight, //aspect ratio
+    1, // near clipping plane
+    1000 // far clipping plane
+  );
+
+  const renderer = new THREE.WebGLRenderer(); //there are other renderers for browsers that don't support webgl, they suck
+  renderer.setSize(window.innerWidth, window.innerHeight); //makes renderer size of the window
+  document.getElementById('webgl').appendChild(renderer.domElement); //append renderer to the DOM
+
+  renderer.render( //render the scene by passing the scene and camera to the renderer
+    scene,
+    camera
+  )
+}
+
+init();
